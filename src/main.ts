839090829035ead0c0136a46c818549df09471d0
sivafastbokz <sivaharshanfastbokz@gmail.com>
2024-01-13 17:33:32 +0530

    updated

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,5 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule,{cors:true});
   await app.listen(5000);
-  // app.enableCors({
-  //   origin: 'http://localhost:3000',
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  //   credentials: true,
-  // })
 }
 bootstrap();
