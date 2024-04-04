e2bc974b681dd01ff1d6910af8ad6e391b4da7a1
sivafastbokz <sivaharshanfastbokz@gmail.com>
2024-04-04 16:05:59 +0530

    updated

--- src/service/salonservice.service.ts
+++ src/service/salonservice.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
-import { SalonService, ServiceDocument  } from './salonservice.schema';
+import { ServiceDocument  } from './salonservice.schema';
 import { CreateServiceDto } from './dto/service.dto';
 
 @Injectable()
